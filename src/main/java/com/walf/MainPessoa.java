969ee7216962c3fe188fe8d71f8db2b9c932e8f2
walfredo <awalfredo@hotmail.com>
2021-12-20 16:39:42 -0300

    add idade

--- src/main/java/com/walf/MainPessoa.java
+++ src/main/java/com/walf/MainPessoa.java
@@ -3,7 +3,7 @@ package com.walf;
 public class MainPessoa {
 
 	public static void main(String[] args) {
-		Pessoa pessoa = new Pessoa("walfredo","almeida");
+		Pessoa pessoa = new Pessoa("walfredo","almeida", 15);
 		Pessoa pessoaa = new Pessoa();
 		
 		System.out.println(pessoa.toString());
--- src/main/java/com/walf/Pessoa.java
+++ src/main/java/com/walf/Pessoa.java
@@ -11,5 +11,6 @@ public class Pessoa {
 	
 	public String nome;
 	public String sobrenome;
+	public int idade;
 
 }
